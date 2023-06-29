ddc865952c792243eaf2fada4c664c56cfad1e68
cooldevsmart <cooldevsmart@gmail.com>
2023-06-29 16:16:15 +0900

    Change the title of main page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "운영팀 웹 어드민 시스템" });
 });
 
 module.exports = router;
